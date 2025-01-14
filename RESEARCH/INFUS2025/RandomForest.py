fc9e7d747097b558118d5f86aec7a469ea82095a
aerenyldrm <aerenyldrm@outlook.com>
2025-01-14 08:54:44 +0200

    update

--- RESEARCH/INFUS2025/RandomForest.py
+++ RESEARCH/INFUS2025/RandomForest.py
@@ -3,9 +3,8 @@ import pandas as pd
 import random as rd
 import seaborn as sns
 import matplotlib.pyplot as plot
-from sklearn.tree import plot_tree
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 # set seed for reproducibility
@@ -25,8 +24,8 @@ print(y) # for debug
 # split data into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
 
-# utilize decision tree regressor
-model = DecisionTreeRegressor(random_state=2)
+# utilize random forest regressor
+model = RandomForestRegressor(n_estimators=100, random_state=2)
 
 # train model
 model.fit(x_train, y_train)
@@ -37,11 +36,7 @@ y_predict = model.predict(x_test)
 # evaluate model
 print(f" Decision Tree Mean Squared Error:\t{mean_squared_error(y_test, y_predict)}")
 
-# visualize tree
-plot.figure(figsize=(16, 9))
-plot_tree(model, feature_names=["Re", "Gr", "Ha", "Rm"], max_depth=3, fontsize=6, filled=True)
-plot.show()
-
+# visualize
 plot.figure(figsize=(16, 9))
 plot.scatter(y_test, y_predict, alpha=0.5, color="black")
 plot.plot([y.min(), y.max()], [y.min(), y.max()], color="black")
